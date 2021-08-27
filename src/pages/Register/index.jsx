f343a1a2f9dc29db54b098de246be801294de55d
Juliana Burzlaff <juliana.burzlaff@hotmail.com>
2021-08-27 16:35:54 -0300

    page register

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import AuthTemplate from '../../components/AuthTemplate';
+import * as S from './styles';
+
+function Register() {
+  return <AuthTemplate sizeLeft={4}>//colocar aqui o formulário</AuthTemplate>;
+}
+
+export default Register;
