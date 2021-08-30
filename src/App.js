3068c52cb610668445c3c554d5b082e2eefe343c
Juliana Burzlaff <juliana.burzlaff@hotmail.com>
2021-08-30 15:25:56 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import { ThemeProvider as MaterialThemeProvider } from '@material-ui/core/styles';
+import {
+  ThemeProvider as MaterialThemeProvider,
+  StylesProvider,
+} from '@material-ui/core/styles';
 import Router from './routes';
 import theme from './theme';
 import AppProvider from './providers';
@@ -9,16 +12,18 @@ import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
-    <MaterialThemeProvider theme={theme}>
-      <ThemeProvider theme={theme}>
-        <AppProvider>
-          <UploadProvider>
-            <Router />
-            <GlobalStyle />
-          </UploadProvider>
-        </AppProvider>
-      </ThemeProvider>
-    </MaterialThemeProvider>
+    <StylesProvider injectFirst>
+      <MaterialThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <AppProvider>
+            <UploadProvider>
+              <Router />
+              <GlobalStyle />
+            </UploadProvider>
+          </AppProvider>
+        </ThemeProvider>
+      </MaterialThemeProvider>
+    </StylesProvider>
   );
 }
 
--- src/components/ForgotPasswd/styles.jsx
+++ src/components/ForgotPasswd/styles.jsx
@@ -19,18 +19,14 @@ export const Form = styled.form`
 `;
 
 export const Title = styled(Typography)`
-  && {
-    font-size: 26px;
-  }
+  font-size: 26px;
 `;
 
 export const Text = styled(Typography)`
-  && {
-    font-size: 20px;
-    width: 400px;
-    text-align: center;
-    margin: 40px 0 20px;
-  }
+  font-size: 20px;
+  width: 400px;
+  text-align: center;
+  margin: 40px 0 20px;
 `;
 
 export const Separation = styled.div`
