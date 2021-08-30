a39fd54084ef78d3b00f4d352ec57150bf85a7fe
Rey <rey.filareto@outlook.com>
2021-08-30 03:14:03 -0300

    Update index.jsx

--- src/components/ForgotPasswd/index.jsx
+++ src/components/ForgotPasswd/index.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import InputMask from 'react-input-mask';
 import MailOutlineIcon from '@material-ui/icons/MailOutline';
 import PhoneIcon from '@material-ui/icons/Phone';
 import { TextField, InputAdornment } from '@material-ui/core';
 
 import * as S from './styles';
+
+const method = { email: undefined, sms: undefined };
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Email inválido')
+    .test('verify-email', 'Escolha um único método de verificação', val => {
+      method.email = val;
+      if ((method.sms && method.email) || (!method.sms && !method.email)) {
+        return false;
+      }
+      return true;
+    }),
+  sms: yup
+    .string()
+    .test('phone-validation', 'Telefone inválido', val => {
+      return !/[^0-9()\s']/.exec(val) && !/[0-9]{11}/.exec(val);
+    })
+    .test('verify-phone', 'Escolha um único método de verificação', val => {
+      method.sms = val;
+      if ((method.sms && method.email) || (!method.sms && !method.email)) {
+        return false;
+      }
+      return true;
+    }),
+});
 
 function ForgotPasswd({ onSuccess }) {
   const history = useHistory();
@@ -15,7 +44,9 @@ function ForgotPasswd({ onSuccess }) {
     register,
     formState: { errors },
     handleSubmit,
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const codeSend = async ({ email, phone }) => {
     console.log(email);
@@ -39,6 +70,7 @@ function ForgotPasswd({ onSuccess }) {
           margin="normal"
           {...register('email')}
           helperText={errors.email?.message}
+          error={errors.email}
           InputProps={{
             startAdornment: (
               <InputAdornment>
@@ -64,8 +96,9 @@ function ForgotPasswd({ onSuccess }) {
               placeholder="telefone"
               fullWidth="true"
               margin="normal"
-              {...register('phone')}
-              helperText={errors.phone?.message}
+              {...register('sms')}
+              helperText={errors.sms?.message}
+              error={errors.sms}
               InputProps={{
                 startAdornment: (
                   <InputAdornment>
